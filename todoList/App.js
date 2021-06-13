4467869dd061acc89b54d6591eabe8140b829ee5
HelloDearSir <jonbarton@hotmail.co.uk>
2021-06-13 21:49:45 +0100

    notifactions

--- todoList/App.js
+++ todoList/App.js
@@ -1,70 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { KeyboardAvoidingView,StyleSheet, Text, TextInput, View,TouchableOpacity,Button } from 'react-native';
-import Task from './components/Task';
-import * as Notifcation from 'expo-notifications';
-export default function App() {
-  const [task, setTask] = useState();
-  const [taskItems, setTaskItems] = useState([]);
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native'; 
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Test from './test';
 
-  const handleAddTask = () => {
-    setTaskItems([...taskItems, task])
-    setTask(null);
-  }
-  const completeTask = (index) => {
-    let itemsCopy = [...taskItems];
-    itemsCopy.splice(index, 1);
-    setTaskItems(itemsCopy)
-  }
+const Tab = createBottomTabNavigator();
 
-  const handleNotifcation = () => {
-    Notifcation.scheduleNotificationAsync({
-      content: {
-        title: "testing into notifications",
-        body: "this is my local shit"
-        },
-        trigger:{
-          seconds: 1,
-        }
-      })
-  };
+ function HomeScreen() {
+   return(
+     <Test/>
+   )
+ }
 
-  return (
-    <View style={styles.container}>
-      <Button title={"open Notifcation"}onPress={handleNotifcation} /> 
-     <View style={styles.taskWrapper}>
-        <Text style={styles.sectionTitle}> Today's Tasks</Text>
-         <View style={styles.items}>
-           {
-              taskItems.map((item, index) => {
-                return (
-                  <TouchableOpacity key={index}  onPress={() => completeTask(index)}>
-                    <Task text={item} /> 
-                  </TouchableOpacity>
-                )
-              })
-           } 
-
-        </View>
-        </View>
-        <KeyboardAvoidingView 
-        behavior={Platform.OS === "android" ? "padding" : "height"}
-        style={styles.writeTaskWrapper}
-      >
-    <TextInput style={styles.input} placeholder={'write a task'} value={task} onChangeText={text => setTask (text)}/>
-
-    <TouchableOpacity onPress={()=> handleAddTask()}>
-     <View style={styles.addWrapper}>
-      <Text style={styles.addText}>+ </Text>
-      </View>
-     </TouchableOpacity>
-  </KeyboardAvoidingView>
-    </View>
-
-
-
-  );
+ function SettingScreen() {
+  return(
+    <Test/>
+  )
 }
+export default function App() { 
+
+   return (
+     <NavigationContainer>
+       <Tab.Navigator tabBarOptions={{
+        
+         style: {
+           backgroundColor:'#fff',
+           position:'absolute',
+           bottom:40,
+           marginHorizontal: 20,
+          height:60,
+          borderRadius:10,
+          shadowColor:'#000',
+          shadowOpacity: 0.06,
+          shadowOffset: {
+            width: 10,
+            height: 10
+          },
+       
+
+         }
+       }}>
+       <Tab.Screen name={"Home"} component={HomeScreen}>
+           </Tab.Screen>
+
+       <Tab.Screen name={"Setting"} component={SettingScreen}></Tab.Screen>
+       </Tab.Navigator>
+      
+     </NavigationContainer>
+   );
+}
+
+
+ 
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ todoList/test.js
@@ -0,0 +1,127 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { KeyboardAvoidingView,StyleSheet, Text, TextInput, View,TouchableOpacity,Button } from 'react-native';
+import Task from './components/Task';
+import * as Notifcation from 'expo-notifications';
+
+Notifcation.setNotificationHandler({
+  handleNotifcation: async () => {
+    return {
+      ShouldPlaySound: true, 
+      shouldShowAlert: true, 
+    }
+  }
+})
+
+const Test= () => {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy)
+  }
+
+  const handleNotifcation = () => {
+    Notifcation.scheduleNotificationAsync({
+      content: {
+        title: "testing into notifications",
+        body: "this is my local shit"
+        },
+        trigger:{
+          seconds: 1,
+        }
+      })
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title={"open Notifcation"}onPress={handleNotifcation} /> 
+     <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}> Today's Tasks</Text>
+         <View style={styles.items}>
+           {
+              taskItems.map((item, index) => {
+                return (
+                  <TouchableOpacity key={index}  onPress={() => completeTask(index)}>
+                    <Task text={item} /> 
+                  </TouchableOpacity>
+                )
+              })
+           } 
+
+        </View>
+        </View>
+        <KeyboardAvoidingView 
+        behavior={Platform.OS === "android" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+    <TextInput style={styles.input} placeholder={'write a task'} value={task} onChangeText={text => setTask (text)}/>
+
+    <TouchableOpacity onPress={()=> handleAddTask()}>
+     <View style={styles.addWrapper}>
+      <Text style={styles.addText}>+ </Text>
+      </View>
+     </TouchableOpacity>
+  </KeyboardAvoidingView>
+    </View>
+
+
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+
+  },
+  sectionTitle: {
+    fontSize: 24,
+
+  },
+  items: {
+    marginTop: 30
+  } ,
+  writeTaskWrapper: {
+    position: 'absolute',
+    top: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    padding: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+   },
+  addWrapper:{
+    width:60,
+    height:60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addText:{},
+
+});
+
+export default Test;
